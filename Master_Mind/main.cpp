3145b56251d9bc642c1e0d26aa97096e35347f23
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-03-23 11:25:32 +0000

    rr

--- Master_Mind/main.cpp
+++ Master_Mind/main.cpp
@@ -1,11 +1,20 @@
 #include "Master.cpp"
 
-
+using namespace std;
 int main(int argc, char const *argv[])
 {
-    Master prova = Master();
+    mastermind prova = mastermind();
+
+    std::cout<<"Benvenuto al gioco mastermind!"<<std::endl;
     
     prova.nuova_giocata();
+    prova.risultato_mossa();
+    
+    do{
+        prova.nuova_giocata();
+        prova.risultato_mossa();
+    }while (prova.puoi_continuare());
+    prova.situazione_finale();
 
     return 0;
 }
