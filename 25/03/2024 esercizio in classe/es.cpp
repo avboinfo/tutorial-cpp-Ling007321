2986fe5657db444a86d2f70600b6856243453763
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-03-25 07:59:29 +0000

    b

---
+++ 25/03/2024 esercizio in classe/es.cpp
@@ -0,0 +1,30 @@
+/*25/03/2024
+**Chen JinXin
+**3°CIN
+**ES N°15 P
+*/
+#include<iostream>
+
+using namespace std;
+
+int sommaN(int n,int n1){
+    int somma=0;
+    int som=0;
+    for(int i=n;i<=n1;i++){
+        
+        somma=somma+i;
+        
+    }
+    
+    return somma;
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    int somma = sommaN(10,15);
+    cout << somma;
+    
+    return 0;
+}
