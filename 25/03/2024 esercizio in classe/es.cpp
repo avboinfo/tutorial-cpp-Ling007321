b162046a51a320be5594c2ef1074aaf722e7c383
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-03-25 08:00:14 +0000

    m

--- 25/03/2024 esercizio in classe/es.cpp
+++ 25/03/2024 esercizio in classe/es.cpp
@@ -3,28 +3,27 @@
 **3°CIN
 **ES N°15 P
 */
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int sommaN(int n,int n1){
-    int somma=0;
-    int som=0;
-    for(int i=n;i<=n1;i++){
-        
-        somma=somma+i;
-        
+int sommaN(int n, int n1)
+{
+    int somma = 0;
+    int som = 0;
+    for (int i = n; i <= n1; i++)
+    {
+
+        somma = somma + i;
     }
-    
+
     return somma;
 }
 
-
-
 int main(int argc, char const *argv[])
 {
-    int somma = sommaN(10,15);
+    int somma = sommaN(10, 15);
     cout << somma;
-    
+
     return 0;
 }
