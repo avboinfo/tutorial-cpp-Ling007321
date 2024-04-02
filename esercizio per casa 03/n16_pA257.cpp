64b238cde9d81c368c0150ce80c9be4e321a3c39
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-04-02 20:15:40 +0000

    2

---
+++ esercizio per casa 03/n16_pA257.cpp
@@ -0,0 +1,21 @@
+/*
+**Esercizio per casa pag A257 N14 per 03/04/2024
+**3°CIN
+**Chen Jin Xin
+*/
+
+include<iostream> using namespace std;
+
+void ricorsione(string c, string s)
+{
+    if (s[c] == c)
+        return 1;
+    else
+        return n + ricorsione(n - 1);
+}
+
+int main(int argc, char const *argv[])
+{
+    cout<< ricorsione;
+    return 0;
+}
