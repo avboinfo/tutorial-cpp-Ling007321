00b2b2e2376f889e7a9d353da49237b10ff11687
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-04-22 07:40:08 +0000

    2204

--- X_BattleShip/BattleShip.cpp
+++ X_BattleShip/BattleShip.cpp
@@ -28,34 +28,53 @@ public:
     }
     void play()
     {
-        for (int i = 0; i < 20; i++)
+
+        while (true)
         {
-            int x = rand() % DIM;
-            int y = rand() % DIM;
-            if (campo.get(x, y) == HIT)
-                continue;
-            if (campo.get(x, y) == SHIP)
-            {
-                mappa.put(x, y, HIT);
-                campo.put(x, y, HIT);
-            }
-            else
-                mappa.put(x, y, MISS);
+            mappa.stampa();
+            if (!playHand())
+                break; //!=not playHand== if playHand ==False
         }
+
+        /* for (int i = 0; i < 20; i++)
+         {
+             int x = rand() % DIM;
+             int y = rand() % DIM;
+             if (campo.get(x, y) == HIT)
+                 continue;
+             if (campo.get(x, y) == SHIP)
+             {
+                 mappa.put(x, y, HIT);
+                 campo.put(x, y, HIT);
+             }
+             else
+                 mappa.put(x, y, MISS);
+         }
+         */
         mappa.stampa();
 
-        ask();
+        playHand();
 
         mappa.stampa();
         campo.stampa();
     }
 
-    void ask()
+    bool playHand()
     {
-        cout << "Inserisci le coordinate di  riga e colonna in cui sganciare la bomba:  " << endl;
+        cout << "Inserisci le coordinate di  riga e colonna tra (1 -" << DIM << ") in cui sganciare la bomba:  (0 per terminare): " << endl;
         int x, y;
         cin >> x;
+
+        if (x <= 0||x>DIM)
+            return false;
+        else
+            x--;
         cin >> y;
+
+        if (y <= 0||x>DIM)
+            return false;
+        else
+            y--;
         if (campo.get(x, y) == SHIP)
         {
             mappa.put(x, y, HIT);
@@ -63,5 +82,20 @@ public:
         }
         else
             mappa.put(x, y, MISS);
+            return true;
     }
+    
+    bool gameOver(){
+        
+        for(int i=0;i<DIM;i++){
+            for(int j=0;j<DIM;j++){
+                if(campo.get(i,j)==SHIP){
+                    
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 };
--- X_BattleShip/Main.cpp
+++ X_BattleShip/Main.cpp
@@ -2,6 +2,8 @@
 **BattleShip.cpp una classe per giocare a battaglia navale
 **Chen JinXin _20/04/2024
 */
+
+//Statico: Esiste da sempre ,come il sole. per tutti o non solo
 #include "BattleShip.cpp"
 #include<iostream>
 using namespace std;
