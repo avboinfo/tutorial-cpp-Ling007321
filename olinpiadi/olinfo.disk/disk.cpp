8b024a2d4cf38d27b9828bfb698aff7381739485
Ling007321 <153986387+Ling007321@users.noreply.github.com>
2024-06-01 09:34:28 +0000

    01061

--- olinpiadi/olinfo.disk/disk.cpp
+++ olinpiadi/olinfo.disk/disk.cpp
@@ -6,26 +6,25 @@
 
 using namespace std;
 
-int main() {
-//  uncomment the following lines if you want to read/write from files
-  ifstream cin("input.txt");
-  ofstream cout("output.txt");
-    int t;
-    int a;
-    int orelavoro;
-    int rimasto;
+int main()
+{
+    //  uncomment the following lines if you want to read/write from files
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
     long long A, B, T;
     cin >> A >> B >> T;
+    long long g_interi = T / 24;
+    long long tempo_online = B - A;
+    long long left_over = te, po_online * g_interi;
+    T = T - _interi * 24;
+    T = T - (24 - tempo_online);
+    if (T < 0)
+        T = 0;
 
-    
-    a=B-A;
-    orelavoro=24-a;
-    rimasto = T-(orelavoro*2);
-
-   
     // insert your code here
 
-    cout << rimasto << endl; // print the result
+    cout << T + left_over << endl; // print the result
 
     return 0;
 }
